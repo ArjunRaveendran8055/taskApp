54347a004803e48c55464eed3641e771d4141e12
Arjun Raveendran <arjunravi1213@gmail.com>
2023-09-07 23:38:11 +0530

    check button remaining

--- src/App.jsx
+++ src/App.jsx
@@ -4,37 +4,49 @@ import "./App.css";
 const App = () => {
   const [input, setInput] = useState("");
   const [tasklist, setTasklist] = useState([]);
-
+  const [textstatus, setTextstatus] = useState(true);
+  const [checkActive,setCheckActive]=useState(false)
   const submitHandler = () => {
-    if (input == "") return;
+    if (input == "") {
+      setTextstatus(false)
+      return;
+    }
     if (!tasklist) return;
-    
-    setTasklist([...tasklist,input]);
-    console.log(tasklist);
+    setTasklist([...tasklist, input]);
     setInput("");
   };
 
   const deleteHandler = (ind) => {
-    console.log("array is",tasklist)
     setTasklist(
       tasklist.filter((item, index) => {
         if (index != ind) {
           return item;
         }
-      }),
+      })
     );
     setInput("");
+  };
+
+  const onChangeHandler = (e) => {
+    setTextstatus(true)
+    setInput(e.target.value.toUpperCase());
+  };
+
+  const checkHandler = (e) => {
+    setCheckActive(true)
   };
 
   return (
     <div className="main-container">
       <div className="form-container">
         <div className="heading">
-          <h1>Let's Finish What You Have<i
-  className="fa-solid fa-cart-arrow-down fa-shake"
-  style={{ color: "#07065a" }}
-/>
-</h1>
+          <h1>
+            Let's Finish What You Have
+            <i
+              className="fa-solid fa-cart-arrow-down fa-shake"
+              style={{ color: "#07065a" }}
+            />
+          </h1>
         </div>
         <div className="add">
           <input
@@ -43,35 +55,47 @@ const App = () => {
             value={input}
             placeholder="Enter task you wish to add...."
             onChange={(e) => {
-              setInput(e.target.value.toUpperCase());
+              onChangeHandler(e);
             }}
+            style={{background:textstatus?"white":"#FFCCCB"}}
           />
           <button className="btn" onClick={submitHandler}>
             Add
           </button>
         </div>
       </div>
-      {tasklist.length==0? (
+      {tasklist.length == 0 ? (
         <div>
           <h1>No Pending Tasks ...</h1>
         </div>
       ) : (
-        
-          tasklist.map((item, index) => {
-            return (
-              <div className="list-row" key={index}>
-                <p className="item">{item}</p>
+        tasklist.map((item, index) => {
+          return (
+            <div className="list-row" key={index}>
+              <p className="item" style={{textDecoration: checkActive? 'line-through':'none'}}>{item}</p>
+              <div className="btn-div">
+                <button className="check-btn" onClick={checkHandler}>
+                  <i
+                    className="fa-solid fa-square-check fa-2xl"
+                    style={{ color: "#076e0e" }}
+                  ></i>
+                </button>
                 <button
                   value={index}
                   className="del-btn"
-                  onClick={(e)=>{deleteHandler(index)}}
+                  onClick={(e) => {
+                    deleteHandler(index);
+                  }}
                 >
-                  delete{console.log(tasklist)}
+                  <i
+                    className="fa-solid fa-trash-can fa-lg"
+                    style={{ color: "#f20707" }}
+                  />
                 </button>
               </div>
-            );
-          })
-        
+            </div>
+          );
+        })
       )}
     </div>
   );
